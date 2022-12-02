c1a9139a12589e3dfee0dbe10b91f7d1f8c90ec7
theburyat <theburyat16@gmail.com>
2022-12-03 00:44:52 +0300

    #2 add ability to play paper-rock-scissors

--- DecisionMakerAPI/Controllers/DecisionsController.cs
+++ DecisionMakerAPI/Controllers/DecisionsController.cs
@@ -20,4 +20,60 @@ public class DecisionsController : ControllerBase
     {
         return ((Coin)_random.Next(0, 2)).ToString();
     }
+
+    [HttpGet("cuefa")]
+    public CuefaGameResult PlayCuefa([FromBody] Cuefa playerChoice)
+    {
+        var enemyChoice = (Cuefa)_random.Next(0, 3);
+        var gameResult = new CuefaGameResult { EnemyChoice = enemyChoice};
+        switch (playerChoice)
+        {
+            case Cuefa.Paper:
+                switch (enemyChoice)
+                {
+                    case Cuefa.Paper:
+                        gameResult.Result = CuefaResult.Draw;
+                        break;
+                    case Cuefa.Rock:
+                        gameResult.Result = CuefaResult.Win;
+                        break;
+                    case Cuefa.Scissors:
+                        gameResult.Result = CuefaResult.Lose;
+                        break;
+                }
+                break;
+
+            case Cuefa.Rock:
+                switch (enemyChoice)
+                {
+                    case Cuefa.Paper:
+                        gameResult.Result = CuefaResult.Lose;
+                        break;
+                    case Cuefa.Rock:
+                        gameResult.Result = CuefaResult.Draw;
+                        break;
+                    case Cuefa.Scissors:
+                        gameResult.Result = CuefaResult.Win;
+                        break;
+                }
+                break;
+            
+            case Cuefa.Scissors:
+                switch (enemyChoice)
+                {
+                    case Cuefa.Paper:
+                        gameResult.Result = CuefaResult.Win;
+                        break;
+                    case Cuefa.Rock:
+                        gameResult.Result = CuefaResult.Lose;
+                        break;
+                    case Cuefa.Scissors:
+                        gameResult.Result = CuefaResult.Draw;
+                        break;
+                }
+                break;
+        }
+
+        return gameResult;
+    }
 }
